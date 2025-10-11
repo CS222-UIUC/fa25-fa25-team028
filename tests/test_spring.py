6143498d526aadb26cd8f3eb4148cb9a145c51a5
Christian165293 <christian.bourquin05@gmail.com>
2025-10-11 12:53:41 -0500

    reformated

--- tests/test_spring.py
+++ tests/test_spring.py
@@ -1,6 +1,8 @@
 import pytest
 import numpy as np
 from engine import Body, Spring
+
+
 class TestSpring:
     def test_initialization(self):
         """Testing that the spring initializes with correct initial values"""
@@ -12,6 +14,7 @@ class TestSpring:
         assert spring.rest_length == 0.5
         assert spring.body1 == body1
         assert spring.body2 == body2
+
     def test_force_at_rest(self):
         """Testing that the spring has no force when at rest"""
         body1 = Body(mass=1.0, position=[0, 0], velocity=[0, 0])
@@ -21,6 +24,7 @@ class TestSpring:
 
         assert np.allclose(body1.force, [0, 0])
         assert np.allclose(body2.force, [0, 0])
+
     def test_force_when_stretched(self):
         """Testing that the springs have inward forces when stretched"""
         body1 = Body(mass=1.0, position=[0, 0], velocity=[0, 0])
@@ -31,6 +35,7 @@ class TestSpring:
         assert body1.force[0] > 0
         assert body2.force[0] < 0
         assert np.isclose(body1.force[0], -body2.force[0])
+
     def test_force_when_compressed(self):
         """Testing that the springs have outward forces when test_force_when_compressed"""
         body1 = Body(mass=1.0, position=[0, 0], velocity=[0, 0])
