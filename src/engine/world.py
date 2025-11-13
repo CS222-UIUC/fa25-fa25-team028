1633114db0987d53388979a29562674c5a9598ab
Christian165293 <christian.bourquin05@gmail.com>
2025-11-13 16:59:13 -0600

    added pygame demo for the multi spring system and adjusted physics for proper integration

--- src/engine/world.py
+++ src/engine/world.py
@@ -20,8 +20,5 @@ class World:
         AIR_RESISTANCE = 0.98  # Multiply velocity by this each step
 
         for body in self.bodies:
-            if body.mass < 1e9:
                 euler_step(body, self.dt)
                 body.velocity *= AIR_RESISTANCE  # Dampen all motion
-            else:
-                body.clear_force()
