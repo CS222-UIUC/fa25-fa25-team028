13b4a4fe9df94de38387bfc5622982170a7233d2
Zonghang <zwu63@illinois.edu>
2025-12-07 17:02:03 +0800

    demo1

--- src/engine/body.py
+++ src/engine/body.py
@@ -1,23 +1,28 @@
 import numpy as np
 
 # mass, position, velocity and force
-
 class Body:
-    def __init__(self, mass, position, velocity, radius=0.5, restitution=1.0, is_static=False):
-        self.mass = mass
+    def __init__(self, mass, position, velocity,
+                 radius=0.5, restitution=1.0, is_static=False):
+        self.mass = float(mass)
         self.position = np.array(position, dtype=float)
         self.velocity = np.array(velocity, dtype=float)
         self.force = np.zeros(2, dtype=float)
+
         self.radius = float(radius)
         self.restitution = float(restitution)
         self.is_static = bool(is_static)
-        self.radius = radius
+
+
+        self.sleeping = False     
+        self.sleep_time = 0.0     
 
     def apply_force(self, force):
         self.force += np.array(force, dtype=float)
 
     def clear_force(self):
-        self.force = np.zeros(2, dtype=float)
+
+        self.force[:] = 0.0
 
     def kinetic_energy(self):
         velocity_magnitude_squared = np.dot(self.velocity, self.velocity)
@@ -32,6 +37,20 @@ class Body:
 
     @property
     def inverse_mass(self):
+        """mass inverter"""
         if self.is_static or not np.isfinite(self.mass):
             return 0.0
         return 1.0 / self.mass
+
+        
+    def put_to_sleep(self):
+        """sleep"""
+        self.sleeping = True
+        self.sleep_time = 0.0
+        self.velocity[:] = 0.0
+        self.clear_force()
+
+    def wake(self):
+        """wake up"""
+        self.sleeping = False
+        self.sleep_time = 0.0
